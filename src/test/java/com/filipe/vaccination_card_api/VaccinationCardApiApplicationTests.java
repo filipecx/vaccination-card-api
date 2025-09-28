10c5df4cedb8fdedd33a05708f74d41e9fa635ae
Filipe C <filipecavalcantiximenes@gmail.com>
2025-09-28 16:32:30 -0300

    :tada:

---
+++ src/test/java/com/filipe/vaccination_card_api/VaccinationCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.vaccination_card_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
